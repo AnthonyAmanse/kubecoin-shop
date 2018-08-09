7d9df39f2eab75c35c467d329f8b948ec8c33e5d
AnthonyAmanse <ghieamanse@gmail.com>
2018-08-09 08:03:10 -0700

    Prep for sweden
    Signed-off-by: AnthonyAmanse <ghieamanse@gmail.com>

--- public/scripts/searchContract.js
+++ public/scripts/searchContract.js
@@ -1,7 +1,7 @@
 /*global document:false alert:false XMLHttpRequest:false */
 
 let BLOCKCHAIN_URL = "https://anthony-blockchain.us-south.containers.mybluemix.net";
-let BLOCKCHAIN_SELLER_ID = "958ea9d6-59bf-46d5-8530-7388c5153a88";
+let BLOCKCHAIN_SELLER_ID = "f7b79706-9f0b-4049-81a8-dc0355401513";
 
 let searchButton = document.getElementById("searchId");
 searchButton.addEventListener("click", searchContractById);
